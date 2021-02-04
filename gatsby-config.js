bb01eab8d529477401e1b4fdd974a04e9ddb4965
Kyle Gill <kylerobertgill@gmail.com>
2021-02-04 15:45:07 -0700

    remove excess dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,17 +3,6 @@ module.exports = {
     title: "super-mario-site",
   },
   plugins: [
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
     "gatsby-source-super-mario",
     {
       resolve: "gatsby-omni-font-loader",
