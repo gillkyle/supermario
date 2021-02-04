98c1eca060c8739660e55a4f397c2d972a5a1e84
Kyle Gill <kylerobertgill@gmail.com>
2021-02-04 15:43:39 -0700

    add styles and font

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,18 @@ module.exports = {
       __key: "images",
     },
     "gatsby-source-super-mario",
+    {
+      resolve: "gatsby-omni-font-loader",
+      options: {
+        enableListener: true,
+        preconnect: ["https://fonts.gstatic.com"],
+        web: [
+          {
+            name: "Press Start 2P",
+            file: "https://fonts.googleapis.com/css2?family=Press+Start+2P",
+          },
+        ],
+      },
+    },
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,13 @@ import { graphql, Link } from "gatsby";
 const ComponentName = ({ data }) => (
   <main>
     <h1>Super Mario Bros</h1>
-    <section>
+    <section
+      style={{
+        display: `grid`,
+        gridTemplateColumns: `repeat(auto-fit, minmax(150px, 1fr))`,
+        gridGap: 16,
+      }}
+    >
       {data.allLevel.nodes.map((level) => (
         <Link to={level.gatsbyPath}>{level.name}</Link>
       ))}
